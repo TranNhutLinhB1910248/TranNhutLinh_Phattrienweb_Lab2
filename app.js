a470bc04896eb3c4f54f0ee0b502576b2973f731
Linh <trannhutlinh0925@gmail.com>
2022-09-21 08:57:01 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
-// const ApiError = require("./app/api-error")
+const ApiError = require("./app/api-error")
 
 const app = express();
 
@@ -9,20 +9,20 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome to contact book application." });
+// });
+
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 
 
-// app.use((err, req, res, next) => {
-//     return res.status(error.statusCode || 500).json({
-//         message: error.message || "Internal Server Error",
-//     });
-// });
-
-
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
 module.exports = app;
