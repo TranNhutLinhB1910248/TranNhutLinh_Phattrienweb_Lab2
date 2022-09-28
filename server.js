b4c0d65192434d7a761da81f0044d1e6867d6983
Linh <trannhutlinh0925@gmail.com>
2022-09-28 08:49:53 +0700

    Cai dat express chay thong bao xin chao

--- server.js
+++ server.js
@@ -19,15 +19,3 @@ async function startServer() {
 }
 
 startServer();
-
-
-
-
-
-// const app = require("./app");
-// const config = require("./app/config");
-
-// const PORT = config.app.port;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}.`);
-// });
