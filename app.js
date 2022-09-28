91fce49f3c2c7aa116bf2f3a5ab91f2ff7c43603
Linh <trannhutlinh0925@gmail.com>
2022-09-28 08:42:33 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-// app.get("/", (req, res) => {
-//     res.json({ message: "Welcome to contact book application." });
-// });
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
 
 
 app.use((err, req, res, next) => {
